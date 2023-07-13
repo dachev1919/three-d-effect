322b345f2181037add8e2daa4311a0e8f44a319e
Dachev <dachev1919@gmail.com>
2023-07-13 14:28:51 +0300

    deploy

--- src/modules/home/components/layers/Layers.jsx
+++ src/modules/home/components/layers/Layers.jsx
@@ -4,9 +4,21 @@ import layer2 from '../../../../common/assets/images/layer-2.png';
 import layer5 from '../../../../common/assets/images/layer-5.png';
 import layer6 from '../../../../common/assets/images/layer-6.png';
 import {useRainAnimation} from "../../../../utils/rain";
+import { useEffect } from "react";
 
 const Layers = () => {
   const RainAnimation = useRainAnimation();
+
+  useEffect(() => {
+    document.addEventListener('mousemove', e => {
+      Object.assign(document.documentElement, {
+        style: `
+          --move-x: ${(e.clientX - window.innerWidth / 2) * -0.005}deg;
+          --move-y: ${(e.clientY - window.innerHeight / 2) * -0.01}deg;
+        `
+      })
+    });
+  }, [])
 
   return (
     <section className={styles.layers}>
@@ -21,7 +33,7 @@ const Layers = () => {
           </div>
         </div>
         <div className={`${styles.item} ${styles['layer-4']}`}>
-          {/*{ RainAnimation }*/}
+          { RainAnimation }
         </div>
         <div className={`${styles.item} ${styles['layer-5']}`} style={{backgroundImage: `url(${layer5})`}}></div>
         <div className={`${styles.item} ${styles['layer-6']}`} style={{backgroundImage: `url(${layer6})`}}></div>
